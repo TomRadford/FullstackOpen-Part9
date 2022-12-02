a3e459816e7ca74b466801fe87f8a7c5bb23377d
Tom Radford <tom@theradford.com>
2022-12-02 11:18:31 +0200

    ex up to 9.9

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import { Request } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors<Request>())
 app.use(express.json());
 
-const PORT = 3000
-
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
+	console.log('Server pinged')
 	res.send('pong')
 })
+
+const PORT = 3001
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`)
