31173202e647251545539424fc9cbaeaa5ee45fb
Tom Radford <tom@theradford.com>
2022-12-11 11:25:44 +0200

    ex. up to 9.13

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,5 +1,6 @@
 import express  from 'express';
 import patientsService from '../services/patientsService';
+import {toNewPatient} from '../utils';
 
 const router = express.Router();
 
@@ -8,8 +9,10 @@ router.get('/', (_req, res) => {
 
 router.post('/', (req, res) => {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     const newPatient = toNewPatient(req.body);
+    const addedPatient = patientsService.addPatient(newPatient);
+    res.send(addedPatient);
   } catch (e) {
     let errorMessage = 'Something went wrong. ';
     if (e instanceof Error) {
--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -1,1 +1,73 @@
-const toNewPatient = //here! 
+import { Gender, NewPatient } from '../src/types';
+
+//type gaurds
+const isString = (text: unknown): text is string => {
+  return (typeof text === 'string' || text instanceof String); 
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param: any): param is Gender => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(param);
+};
+
+//parsers
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const parseName = (name: any): string => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  if (!name || !isString(name)) {
+    throw new Error('Incorrect or missing name');
+  }
+  return name;
+};
+
+const parseDateOfBirth = (dob: unknown): string => {
+  if (!dob || !isString(dob) || !isDate(dob)) {
+    throw new Error('Incorrect or missing date of birth');
+  }
+  return dob;
+};
+
+const parseSSN = (ssn: unknown): string => {
+  if (!ssn || !isString(ssn)) {
+    throw new Error('Incorrect or mssing SSN');
+  }
+  return ssn;
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing Gender');
+  }
+  return gender;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error('Incorrect or missing Occupation');
+  }
+  return occupation;
+};
+
+type Fields = {
+name: unknown,
+dateOfBirth: unknown,
+ssn: unknown,
+gender: unknown,
+occupation: unknown
+}
+//params could also be any here instead of fields
+export const toNewPatient = ({name, dateOfBirth, ssn, gender, occupation}:Fields): NewPatient => {
+  return {
+    name: parseName(name),
+    dateOfBirth: parseDateOfBirth(dateOfBirth),
+    ssn: parseSSN(ssn),
+    gender: parseGender(gender),
+    occupation: parseOccupation(occupation)
+  };
+};
+
